bcf9ea1d99d7ade454cd4f1e846a25439e3b42dc
nghialt <luuthanhnghia2004@gmail.com>
2025-07-02 21:29:58 +0700

    up code

--- src/main/java/com/hotel/hotel_stars/Config/VNPayService.java
+++ src/main/java/com/hotel/hotel_stars/Config/VNPayService.java
@@ -47,7 +47,8 @@ public class VNPayService {
         vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
 
         // ✅ Thời gian tạo và hết hạn
-        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+        TimeZone timeZone = TimeZone.getTimeZone("Asia/Ho_Chi_Minh");
+        Calendar cld = Calendar.getInstance(timeZone);
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
         String vnp_CreateDate = formatter.format(cld.getTime());
         vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
